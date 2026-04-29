0a8f1f751fba3c71adab74266deaa8b63c153e7e
Yhsanave <evanashy@gmail.com>
2026-04-28 19:35:27 -0500

    add requirements.txt

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 
+
 class Message(BaseModel):
     content: str
     webhookURL: str
 
+
 app = FastAPI()
 
-@app.post('/discord-forward')
+
+@app.post('/')
 def forward(message: Message):
     res = requests.post(
         url=message.webhookURL,
