19f928bd545fe07ec135d5c32798a6818f262e32
Yhsanave <evanashy@gmail.com>
2026-04-28 19:25:52 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+class Message(BaseModel):
+    content: str
+    webhookURL: str
+
+app = FastAPI()
+
+@app.post('/discord-forward')
+def forward(message: Message):
+    res = requests.post(
+        url=message.webhookURL,
+        data=message.content,
+        headers={
+            'Content-Type': 'application/json'
+        }
+    )
+    res.raise_for_status()
